c8fe5470bbdf486e807061d8f42a37aa30570e8d
Chiel Kunkels <chiel@kunkels.me>
2022-06-06 20:23:28 +0200

    fix(ci): debug action

--- .github/actions/job-matrices/index.js
+++ .github/actions/job-matrices/index.js
@@ -1,13 +1,32 @@
 const core = require('@actions/core');
 
 const resolvers = require('./resolvers');
+console.log('RESOLVERS', resolvers);
 
 try {
-	const packages = core.getInput('packages');
-	console.log(`PACKAGES: ${packages}`);
-	console.log('RESOLVERS', resolvers);
+	const parsedPackages = core.getInput('packages').split(',');
+	console.log(`PACKAGES: ${parsedPackages}`);
 
-	core.setOutput('matrices', {});
+	const packages = parsedPackages.map(pkgName => {
+		const pkgDir = pkgName.replace(/^[^\/]+\//, '');
+		return {
+			name: pkgName,
+			path: path.resolve(process.cwd(), 'packages', pkgDir),
+			dir: pkgDir,
+		};
+	});
+
+	console.log('PACKAGES', packages);
+
+	const jobs = Object.entries(resolvers).reduce((acc, [job, resolver]) => {
+		const jobPackages = packages.filter(pkg => resolver(pkg)).map(pkg => pkg.dir);
+		if (!jobPackages.length) return acc;
+		return { ...acc, [job]: jobPackages };
+	}, {});
+
+	console.log('JOBS', jobs);
+
+	core.setOutput('job_matrices', {});
 } catch (error) {
 	core.setFailed(error.message);
 }
